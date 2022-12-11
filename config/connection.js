1a16b3045f08c25378220c3e7ff295363e590f00
William James <Williamj3795@gmail.com>
2022-12-11 16:35:32 -0500

    change in syntax

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
 let sequelize;
@@ -6,7 +7,8 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,
+     {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
