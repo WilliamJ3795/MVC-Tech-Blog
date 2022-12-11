ee1cb1be5db89264fd1eec35584924fca248fc4b
William James <Williamj3795@gmail.com>
2022-12-11 16:57:22 -0500

    syntax changed

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({helpers});
-
-const exphbs = require('express-handlebars');
-
 const path = require('path');
 const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({
+    helpers
+});
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const routes = require('./controllers');
+
 const sess = {
     secret: "Secret",
     cookie: {},
